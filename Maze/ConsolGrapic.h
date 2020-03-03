d5e295b9a96b35ae271390e3967aeec4f494468d
LaiTial <39575609+LaiTial@users.noreply.github.com>
2020-03-04 00:40:28 +0900

    Delete ConsolGrapic.h

--- Maze/ConsolGrapic.h
+++
@@ -1,9 +0,0 @@
-#include "Consola.h"
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-void xyputc(int x, int y, char ch);
-void fillbox(int x1, int y1, int x2, int y2, char color);
-void xyputstr(int x, int y, string text, int textColor, int textBackGroundColor);
